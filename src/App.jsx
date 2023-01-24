3a79b67a861fd0e031711297cbee95a99da05390
thiago <tmg3.mn@gmail.com>
2023-01-24 11:34:47 -0300

    JSX e estilos implementados no componente raiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="pageContainer">
+        <div className="leftColumn">
+          <img src='assets/img/image-product-desktop.jpg' alt="" />
+        </div>
+        <div className="rightColumn">
+          <div className="content">
+            <h2>Perfume</h2>
+            <h1>Gabrielle Essence Eau de Parfum</h1>
+            <p>A floral, solar and voluptuous inerpretation composed by Olivier Polge, Perfumer-Creator for the House of Chanel</p>
+            <div className="price">
+              <h1 className="price">$149.99</h1>
+              <h3 className="oldPrice">$169.99</h3>
+            </div>
+            <button className='buyBtn'>
+              <img src="assets/img/icon-cart.svg" alt="" />
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
